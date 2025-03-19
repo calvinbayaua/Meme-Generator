ec3cd96cb620c7cc700ed9f700f4c4b7c1c2068f
calvinbayaua <38298812+calvinbayaua@users.noreply.github.com>
2025-03-19 11:03:31 -0700

    Implemented useEffect to handle fetch request to imgflip API

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,60 @@
+import { useState, useEffect } from "react";
+
 export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walkin into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleClick() {
+    const random = Math.floor(Math.random() * allMemes.length);
+    setMeme((prev) => ({ ...prev, imageUrl: allMemes[random].url }));
+  }
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={handleClick}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
